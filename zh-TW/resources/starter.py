d9ea456f3215fdf58ab81ebe7c37eca035751c96
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-05-28 12:39:04 +0100

    New translations starter.py (Chinese Traditional)

--- zh-TW/resources/starter.py
+++ zh-TW/resources/starter.py
@@ -3,6 +3,6 @@ import turtle
 screen = turtle.Screen()
 screen.register_shape('sad.png') 
 turtle.shape('sad.png')
-turtle.setheading(90) # Turtles face right by default
+turtle.setheading(90) #默認情況下烏龜面向右邊
 
-# Try 'happy.png'
+＃試試看'happy.png'
