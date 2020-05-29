287938e57d6d122ed5ccac6594b82138a2fbedc6
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-05-29 13:23:29 +0100

    Add translations
    Ukrainian

---
+++ uk-UA/resources/starter.py
@@ -0,0 +1,8 @@
+import turtle
+
+screen = turtle.Screen()
+screen.register_shape('sad.png') 
+turtle.shape('sad.png')
+turtle.setheading(90) # Спочатку обличчя повернуте вправо
+
+# Спробуй 'happy.png'
