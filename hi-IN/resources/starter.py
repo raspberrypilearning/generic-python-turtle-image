9cd728d14cc6c5396bfc2d4bbeaed971ed43107c
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-06-07 11:50:45 +0100

    New translations starter.py (Hindi)

--- hi-IN/resources/starter.py
+++ hi-IN/resources/starter.py
@@ -3,6 +3,6 @@ import turtle
 screen = turtle.Screen()
 screen.register_shape('sad.png') 
 turtle.shape('sad.png')
-turtle.setheading (90) # Turtle डिफ़ॉल्ट रूप से सही होते हैं
+turtle.setheading(90) # Turtles के मुख डिफ़ॉल्ट रूप से दाहिने ओर होते हैं
 
 # आजमाएं 'happy.png'
